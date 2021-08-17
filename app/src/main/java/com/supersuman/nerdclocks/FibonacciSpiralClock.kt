1ea4f61da0d5c728cb25fc72480b9b6b8d94a1c4
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-08-17 13:21:32 +0530

    Fibonacci Spiral Clock

---
+++ app/src/main/java/com/supersuman/nerdclocks/FibonacciSpiralClock.kt
@@ -0,0 +1,20 @@
+package com.supersuman.nerdclocks
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+
+class FibonacciSpiralClock : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+}
+
+private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val views = RemoteViews(context.packageName, R.layout.fibonacci_spiral_clock)
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
