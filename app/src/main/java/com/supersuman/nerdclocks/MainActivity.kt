4228a11d2313366e26c6d059a298ae8677a73125
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-08-19 22:29:54 +0530

    v1.2
    - Added intent to IgonoreBatteryOptimisations
    - Fibonacci Spiral Clock
    - Binary Analogue Clock

--- app/src/main/java/com/supersuman/nerdclocks/MainActivity.kt
+++ app/src/main/java/com/supersuman/nerdclocks/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.supersuman.nerdclocks
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button : Button = findViewById(R.id.buttonMain)
+        button.setOnClickListener {
+            val intent = Intent()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                intent.action = Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+                startActivity(intent)
+            }
+        }
     }
 }
