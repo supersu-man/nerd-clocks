b2df1d5c2251ed15422aa17ac3e923fd32619e0b
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-06-25 23:55:51 +0530

    v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.supersuman.nerdclocks"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.0.1"
+        versionCode = 6
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
